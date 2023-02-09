ac89ed9c6d0ea4a2367b1e9ddbbf7ef4eae6ec34
andrejcamillo <andrecamillo867@gmail.com>
2023-02-09 17:51:02 -0300

    Alteracoes na tela de lista

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -9,28 +9,59 @@ class TodoListPage extends StatelessWidget {
         body: Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Adicione uma tarefa',
-                        hintText: 'Ex: Estudar Flutter',
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Adicione uma tarefa  teste',
+                            hintText: 'Ex: Estudar Flutter',
+                          ),
+                        ),
                       ),
-                    ),
+                      SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xff00d7f2),
+                          padding: EdgeInsets.all(14),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          size: 30,
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
+                  SizedBox(height: 16),
+                  ListView(
+                    shrinkWrap: true,
+                    children: [
+                        Text('Texto'),
+                      ElevatedButton(onPressed: ()  {},
+                          child: Text('Botao')),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Você possui 0 tarefas pendentes',
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      ElevatedButton(onPressed: ()  {},
+                        style: ElevatedButton.styleFrom(
                         primary: Color(0xff00d7f2),
                         padding: EdgeInsets.all(14),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        size: 30,
-                      ),
+                         ),
+                          child: Text('Limpar tudo')),
+                    ],
                   )
                 ],
               ),
