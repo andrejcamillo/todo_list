de55ec677c3f8ce32b14c9da40ac22168edf0498
andrejcamillo <andrecamillo867@gmail.com>
2023-02-12 22:40:10 -0300

    Criado interacao da lista

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -15,72 +15,77 @@ class _TodoListPageState extends State<TodoListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: todoController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Adicione uma tarefa  teste',
-                        hintText: 'Ex: Estudar Flutter',
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Adicione uma tarefa  teste',
+                          hintText: 'Ex: Estudar Flutter',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: () {
-                      String text = todoController.text;
-                      setState(() {
-                        todos.add(text);
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xff00d7f2),
-                      padding: EdgeInsets.all(14),
-                    ),
-                    child: Icon(
-                      Icons.add,
-                      size: 30,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 16),
-              Flexible(
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    for (String todo in todos) TodoListItem(),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      'Você possui 0 tarefas pendentes',
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                      onPressed: () {},
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        String text = todoController.text;
+                        setState(() {
+                          todos.add(text);
+                        });
+                      },
                       style: ElevatedButton.styleFrom(
                         primary: Color(0xff00d7f2),
                         padding: EdgeInsets.all(14),
                       ),
-                      child: Text('Limpar tudo')),
-                ],
-              )
-            ],
+                      child: Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 16),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (String todo in todos)
+                        TodoListItem(
+                          tittle: todo,
+                        ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Você possui ${todos.length} tarefas pendentes',
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xff00d7f2),
+                          padding: EdgeInsets.all(14),
+                        ),
+                        child: Text('Limpar tudo')),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key}) : super(key: key);
+  const TodoListItem({Key? key, required this.tittle}) : super(key: key);
+
+  final String tittle;
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[200],
-      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        color: Colors.grey[200],
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 2),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('02/01/2023'),
+          Text(
+            tittle,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+           ),
+          ),
+        ],
+      ),
     );
   }
 }
